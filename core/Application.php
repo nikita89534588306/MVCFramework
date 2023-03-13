a1da556bdb054c6ef297478c97238b56e14e11c0
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-13 17:03:08 +0500

    создаем функцию Application->run (выводит результат работы приложения)

--- core/Application.php
+++ core/Application.php
@@ -4,12 +4,18 @@
 		public Request $request;
 		public Router $router;
 		public function __construct(){
-			// echo "Приложение созданно!<br/>";
-			$this->request = new Request(); //создаем экземпляр класса Запрос(Request)
-				// echo "Путь из URL строки : " .$request->getPath() ."<br/>"; 	// выводим URI(для отладки)
-				// echo "Метод запроса : " .$request->getMethod() ."<br/>"; // выводим метод запроса(для отладки)
 			
+			$this->request = new Request(); //создаем экземпляр класса Запрос(Request)
+
 			$this->router = new Router($this->request); //создаем экземпляр класса Маршрутиризатор(Router)
 														//и передаем экземпляр класса Запрос для получения текущего состояния URI 
+		}
+		//данная функция выводи результат работы приложения
+		public function run(){
+			// echo "Приложение созданно!<br/>";
+			// echo "Путь из URL строки : " .$request->getPath() ."<br/>"; 	// выводим URI(для отладки)
+			// echo "Метод запроса : " .$request->getMethod() ."<br/>"; // выводим метод запроса(для отладки)
+			$this->router->resolve(); //данная функция выводит результат работы функции обратного вызова зарегистрированного в Маршрутиризаторе
+			
 		}
 	}
