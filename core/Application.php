ce32b7f2f4fb77e5ade0be1ce20bd4bd91521c4a
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-13 16:23:17 +0500

    создан класс Маршрутиризатор(Router). Класс Маршрутиризатор создается в конструкторе класса Приложение

--- core/Application.php
+++ core/Application.php
@@ -2,12 +2,14 @@
 	namespace app\core;
 	class Application{
 		public Request $request;
+		public Router $router;
 		public function __construct(){
 			// echo "Приложение созданно!<br/>";
-			$request = new Request(); //создаем экземпляр класса Запрос(Request)
-				echo "Путь из URL строки : " .$request->getPath() ."<br/>"; 	// выводим URI(для отладки)
-				echo "Метод запроса : " .$request->getMethod() ."<br/>"; // выводим метод запроса(для отладки)
+			$this->request = new Request(); //создаем экземпляр класса Запрос(Request)
+				// echo "Путь из URL строки : " .$request->getPath() ."<br/>"; 	// выводим URI(для отладки)
+				// echo "Метод запроса : " .$request->getMethod() ."<br/>"; // выводим метод запроса(для отладки)
 			
+			$this->router = new Router();
 			
 		}
 	}
---
+++ core/Router.php
@@ -0,0 +1,5 @@
+<?php 
+	namespace app\core;
+	class Router{
+
+	}
