7775fa015838359d1c32aeb65a39f48cce2d58c0
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-13 16:57:30 +0500

    реалезован метод Маршрутиризатора который в зависимости от параметров строки URI вызывает функцию обратного вызова

--- core/Router.php
+++ core/Router.php
@@ -6,8 +6,26 @@
 			$this->request = $request;
 		}
 		protected array $routes = []; //содержит в себе имя метода, путь и функцию обратного вызова
+		
 		public function get($path, $callback){
 			$this->routes['get'][$path] = $callback;
+		}
+
+		//данная функция вызывает зарегистрированную функция обратного вызова в зависимости от параметров строки URI
+		public function resolve(){ 
+			$currentPath = $this->request->getPath();		//получем текущий путь
+			$currentMethod = $this->request->getMethod();	//получем текущий метод
+			
+			//если существует функция обраного вызова для данного метода и маршрута
+			//то присваеваем её переменной $callback
+			//если такой функции нет то присваиваем переменной строку "Not found"
+			$callback = $this->routes[$currentMethod][$currentPath] ?? "Not found";
+			if($callback === "Not found") { //если функция не найдена
+				echo "Not found"; //выводи сообщение об ошибке
+				exit();	
+			}
+			else
+				echo call_user_func($callback);
 		}
 
 		
