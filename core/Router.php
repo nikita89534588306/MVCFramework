5c236b34ea0587774078be35f060cb58b0e557ab
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-13 16:43:44 +0500

    связываем Маршрутиризатор с классом Запрос внутри Приложения

--- core/Router.php
+++ core/Router.php
@@ -1,8 +1,14 @@
 <?php 
 	namespace app\core;
 	class Router{
+		public Request $request;
+		public function __construct(Request $request){
+			$this->request = $request;
+		}
 		protected array $routes = []; //содержит в себе имя метода, путь и функцию обратного вызова
 		public function get($path, $callback){
 			$this->routes['get'][$path] = $callback;
 		}
+
+		
 	}
