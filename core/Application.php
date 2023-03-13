880dde1a351c9d30e8d7f5260a166de2d537e183
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-13 16:16:22 +0500

    реализован метод класса Запрос - getPath(получить путь)

--- core/Application.php
+++ core/Application.php
@@ -5,7 +5,7 @@
 		public function __construct(){
 			// echo "Приложение созданно!<br/>";
 			$request = new Request(); //создаем экземпляр класса Запрос(Request)
-				echo $request->getPath() ."<br/>"; 	// выводим URI(для отладки)
+				echo "Путь из URL строки : " .$request->getPath() ."<br/>"; 	// выводим URI(для отладки)
 				echo "Метод запроса : " .$request->getMethod() ."<br/>"; // выводим метод запроса(для отладки)
 			
 			
--- core/Request.php
+++ core/Request.php
@@ -5,7 +5,11 @@
 
 		}
 		public function getPath(){
-			return "Путь из строки ULR";
+			$path = $_SERVER['REQUEST_URI'] ?? '/'; //если в URI указан то помещаем его в переменнную $path,
+													//иначе "/" - корневая директория
+			if(strpos($path, '?') === false) //если в URI отсутствуют параметры передаваемые методом GET
+				return $path; //возвращаем путь без изменения
+			else return substr($path, 0, strpos($path, '?')); // удаляем параметры GET запроса и возвращаем строку
 		}
 		public function getMethod(){
 			return strtolower($_SERVER['REQUEST_METHOD']);
