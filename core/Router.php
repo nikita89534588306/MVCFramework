96167bd3c88768d57a45db48d2a10fd0e1c79337
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-13 16:34:39 +0500

    создан метод Маршрутиризатора для регистрации функции которая будет выполняться в зависимости от пути (для метода GET)

--- core/Router.php
+++ core/Router.php
@@ -2,5 +2,7 @@
 	namespace app\core;
 	class Router{
 		protected array $routes = []; //содержит в себе имя метода, путь и функцию обратного вызова
-		
+		public function get($path, $callback){
+			$this->routes['get'][$path] = $callback;
+		}
 	}
