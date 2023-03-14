a550ec77c57c5d283d9ce7ecbc8c7e8f3183181c
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-14 09:05:41 +0500

    переносим index.php в папку public для изоляции приложения

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 	//echo "Точка входа в приложение index.php"; //Файл индекс является единой точкой входа в приложение.
 	
 	//Подключаем автозагрузчик Composer
-	require_once __DIR__ . "/vendor/autoload.php";
+	require_once __DIR__ . "/../vendor/autoload.php";
 	//Указываем пространство имен и создаем экземпляр класса Приложение
 	use app\core\Application;
 	$app = new Application();
