35753afeaf253211c3bfc373eba46c8807a83b2d
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-15 11:19:45 +0500

    создан контроллер для регистрации и входа

---
+++ controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+	namespace app\controllers;
+	use app\core\Controller;
+	class AuthController extends Controller{
+		public function login(){
+			return $this->render('login');
+		}
+		public function register(){
+			return $this->render('register');
+		}
+		
+	}
--- public/index.php
+++ public/index.php
@@ -5,7 +5,8 @@
 	require_once __DIR__ . "/../vendor/autoload.php";
 	//Указываем пространство имен и создаем экземпляр класса Приложение
 
-	use app\controllers\SiteController;
+use app\controllers\AuthController;
+use app\controllers\SiteController;
 	use app\core\Application;
 
 	$app = new Application(dirname(__DIR__));
@@ -14,5 +15,9 @@
 	$app->router->get('/', [SiteController::class, 'home']);
 	$app->router->get('/contact', [SiteController::class, 'contact']);
 	$app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+	$app->router->get('/login', [AuthController::class, 'login']);
+	$app->router->post('/register', [AuthController::class, 'register']);
+
 
 	$app->run(); //запуск приложение на выполнение
