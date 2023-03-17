76391d81103ce7776ef4c9d764192a08ecf51a49
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 14:42:38 +0500

    добавленны некоторые константы для правил валидации

--- core/Model.php
+++ core/Model.php
@@ -3,6 +3,13 @@
 	//вне зависимости от конкретной реализации
 	namespace app\core;
 	abstract class Model{
+
+		public const RULE_REQUIRED = "required";
+		public const RULE_EMAIL = "email";
+		public const RULE_MIN = "min";
+		public const RULE_MAX = "max";
+		public const RULE_MATCH = "match";
+
 		public function loadData($data){
 			foreach($data as $nameInputParam => $valueParam){ //перебираем массив данных полученных из аргумента функции и...
 				if(property_exists($this, $nameInputParam)){ //...проверяем есть ли внутри модели поле с именем совпадающим с именем текущего параметра 
