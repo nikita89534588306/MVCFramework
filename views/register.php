9b583ad694b5f9b17da74fb34c76418bbbdda57a
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 09:28:47 +0500

    редактируем форму для регистрации

--- views/register.php
+++ views/register.php
@@ -2,16 +2,24 @@
 
 <form action="" method="post">
   <div class="mb-3">
-    <label for="subject" class="form-label">Subject</label>
-    <input type="text" name="subject" class="form-control" >
+    <label for="firstname" class="form-label">Firstname</label>
+    <input type="text" name="firstname" class="form-control" >
+  </div>
+  <div class="mb-3">
+    <label for="lastname" class="form-label">Lastname</label>
+    <input type="text" name="lastname" class="form-control" >
   </div>
   <div class="mb-3">
     <label for="emain" class="form-label">Email</label>
     <input type="text" name="email" class="form-control" >
   </div>
   <div class="mb-3">
-    <textarea type="text" name="subject" class="form-control" ></textarea>
+    <label for="password" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control" >
   </div>
-
+  <div class="mb-3">
+    <label for="confirmPassword" class="form-label">Confirm password</label>
+    <input type="password" name="confirmPassword" class="form-control" >
+  </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
