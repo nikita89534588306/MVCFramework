15eb3f88926e0c31fe7740c5ddc99a78fcc7bd23
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 13:37:04 +0500

    в классе модели регистрации добавляем поля в которые будут добавленные данные принятые из формы регистрации

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -1,5 +1,12 @@
 <?php
 	namespace app\models;
 	class RegisterModel{
+		public string $firstname;
+		public string $lastname;
+		public string $email;
+		public string $password;
+		public string $confirmPassword;
+		
+		
 		
 	}
