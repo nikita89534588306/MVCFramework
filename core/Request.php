5301cfe23aff730fdcd2851cabb93677194f3462
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 09:17:36 +0500

    добавлены методы для получения информации об HTTP запросе(isGet, isPost)

--- core/Request.php
+++ core/Request.php
@@ -14,6 +14,12 @@
 		public function method(){
 			return strtolower($_SERVER['REQUEST_METHOD']);
 		}
+		public function isGet(){
+			return $this->method()==='get';
+		}
+		public function isPost(){
+			return $this->method()==='post';
+		}
 		public function getBody(){
 			$body = [];
 			if($this->method() == 'get'){
