69d9bc86bcac0c1de9e24ca18dbd992127befa76
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 09:22:02 +0500

    пишем логику для функции контроллера регистрации

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,11 +1,14 @@
 <?php
 	namespace app\controllers;
 	use app\core\Controller;
+use app\core\Request;
+
 	class AuthController extends Controller{
 		public function login(){
 			return $this->render('login');
 		}
-		public function register(){
+		public function register(Request $request){
+			if($request->isPost()) return "Hendeling data user";
 			return $this->render('register');
 		}
 		
