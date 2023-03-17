cacad8a71eac68a5b5b45f06f188cf267e903d4a
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 13:34:33 +0500

    создан класс модели регистрации и её экземпляр в контроллере Авторизации

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 	namespace app\controllers;
 	use app\core\Controller;
-use app\core\Request;
+	use app\core\Request;
+	use app\models\RegisterModel;
 
 	class AuthController extends Controller{
 		public function login(){
@@ -9,9 +10,7 @@ use app\core\Request;
 			return $this->render('login');
 		}
 		public function register(Request $request){
-			// контроллер регистрации должен обрабатывать 2 метода - get и post,
-			// по этому мы должны получить класс Запрос в качестве аргумента и
-			// проанализировав метод выполняем действие
+			$registerModel = new RegisterModel();
 			if($request->isPost()) return "Hendeling data user";
 			$this->setLayout("auth");
 			return $this->render('register');
