a07dcc16e9723d25c0468f16c8dd540ccb7072ec
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-17 13:59:50 +0500

    обозначен алгоритм работы с данными в контроллере

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -10,10 +10,29 @@
 			return $this->render('login');
 		}
 		public function register(Request $request){
-			$registerModel = new RegisterModel();
-			if($request->isPost()) return "Hendeling data user";
-			$this->setLayout("auth");
-			return $this->render('register');
+			
+			
+		$registerModel = new RegisterModel(); //создаем модель для регистрации 
+
+		if($request->isPost()) { //если метод HTTP - POST
+			$registerModel->loadData($request->getBody()); //загрузаем данные в модель (данные и Запроса)
+			
+			if($registerModel->validate()  	// если пройдена валидация
+			&& $registerModel->register() 	//и данные добавленны в бд(метод register уникальный для модели регистрации и описан в нутри класса) 
+			) return "Success";  //выводим сообщение об успешной регистрации
+
+			return $this->render('register', //если регистрация не пройдена то выводим форму регистрации заново
+				//и передаем данные из модели в форму регистрации 
+				['model' => $registerModel ]
+			);
 		}
+
+		//вывод представления для метода GET
+			$this->setLayout("auth");
+			return $this->render('register',
+				['model' => $registerModel ]
+			);
+		
+	}
 		
 	}
