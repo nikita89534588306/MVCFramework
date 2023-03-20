0e83942fe04ce626e0ea4621a7b7a3c47a13f1cc
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-20 15:23:05 +0500

    создан классовый компонент Form и изменено представление с использованием этого компонента

--- core/form/Form.php
+++ core/form/Form.php
@@ -1,10 +1,15 @@
 <?php
 	namespace app\core\form;
+	use app\core\Model;
 	class Form{
 		public static function begin($action, $method){
-			return sprintf('<form action="%s" method="%s">',$action, $method);
+			echo sprintf('<form action="%s" method="%s">',$action, $method);
+			return new Form();
 		}
 		public static function end(){
-			return "</form>";
+			echo "</form>";
+		}
+		public function field(Model $model, $atribute){
+			return new Field($model, $atribute);
 		}
 	}
--- views/register.php
+++ views/register.php
@@ -1,34 +1,14 @@
 <h1>Create an account</h1>
-<?php echo app\core\form\Form::begin('', 'post'); ?>
-<form action="" method="post">
-	<div class="row">
-		<div class="col">  
-			<div class="mb-3">
-				<label for="firstname" class="form-label">Firstname</label>
-				<input type="text" name="firstname" class="form-control <?php echo $model->hasError('firstname') ? ' is-invalid' : '' ?>" 
-					value="<?php echo $model->firstname?>" >
-				<div class="invalid-feedback"><?php echo $model->getFirstError('firstname')?></div>
-			</div>
-		</div>
-		<div class="col">
-			<div class="mb-3">
-				<label for="lastname" class="form-label">Lastname</label>
-				<input type="text" name="lastname" class="form-control" >
-			</div>
-		</div>
-	</div>
+<?php $form =  app\core\form\Form::begin('', 'post'); ?>
 
-  <div class="mb-3">
-    <label for="emain" class="form-label">Email</label>
-    <input type="text" name="email" class="form-control" >
-  </div>
-  <div class="mb-3">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" name="password" class="form-control" >
-  </div>
-  <div class="mb-3">
-    <label for="confirmPassword" class="form-label">Confirm password</label>
-    <input type="password" name="confirmPassword" class="form-control" >
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+	<div class="row">
+		<div class="col"><?php echo $form->field($model, 'firstname');?></div>
+		<div class="col"><?php echo $form->field($model, 'lastname');?></div>
+	</div>
+		
+	<?php echo $form->field($model, 'email');?>
+	<?php echo $form->field($model, 'password')->passwordField();?>
+	<?php echo $form->field($model, 'confirmPassword')->passwordField();?>
+	<?php echo "<button type='submit' class='btn btn-primary'>Submit</button>";?>
+<?php $form->end();?>
+
