ff6b4012c9b3d1632dd06d6767b48c37a1731efe
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-23 13:31:21 +0500

    внутри класса База данных создан экземпляр класса PDO

--- core/Database.php
+++ core/Database.php
@@ -1,5 +1,16 @@
 <?php
 	namespace app\core;
 	class Database{
-		
+		public \PDO $pdo;
+		public function __construct()
+		{
+			$this->pdo = new \PDO(
+				$dsn,
+				$user,
+				$password
+			);
+			$this->pdo->setAttribute(
+				\PDO::ATTR_ERRMODE,
+				\PDO::ERRMODE_EXCEPTION);
+		}
 	}
