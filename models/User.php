62396ea221f33d8b28afe2f497eddf98405bfb77
Nikita Balashov <nikita.89534588306@gmail.com>
2023-03-24 10:37:34 +0500

    добавлен функционал изменения статуса пользователя

--- models/User.php
+++ models/User.php
@@ -5,14 +5,20 @@ use app\core\DbModel;
 
 	
 	class User extends DbModel{
+
 		public string $firstname = '';
 		public string $lastname = '';
 		public string $email = '';
 		public string $password = '';
 		public string $confirmPassword = '';
 
+		const STATUS_INACTIVE = 0;
+		const STATUS_ACTIVE = 1;
+		const STATUS_DELETED = 2;
+		public int $status = self::STATUS_INACTIVE; 
+
 		public function tableName():string{ return "users";}
-		public function attributes(): array { return ['firstname', 'lastname', 'email', 'password']; }
+		public function attributes(): array { return ['firstname', 'lastname', 'email', 'password', 'status']; }
 
 		public function rules():array{ //правила для валидации данных
 			return [
@@ -32,6 +38,7 @@ use app\core\DbModel;
 		}
 		
 		public function save() { //функция регистрации данных пользователя в БД
+			$this->status = self::STATUS_DELETED;
 			$this->password = password_hash($this->password,PASSWORD_DEFAULT);
 			return parent::save();
 		}
